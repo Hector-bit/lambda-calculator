23776f0ea78de9231afa9454fe322a2cb17ac9fa
Hector-bit <hectormartinez.2978@gmail.com>
2019-09-11 01:35:53 -0700

    adding styling to the buttons

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -9,10 +9,10 @@ import SpecialButton from "./SpecialButton";
 const Specials = () => {
   // STEP 2 - add the imported data to state
   const [spec, setSpec] = useState(specials);
-  console.log(specials);
+  //console.log(specials);
 
   return (
-    <div>
+    <div className = "special_container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/
